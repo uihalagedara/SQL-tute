75711e589b658b8049d570bf393a2617877a2b09
uihalagedara <it20224820@my.sliit.lk>
2022-07-05 21:22:26 +0530

    first commit

---
+++ practice.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE record_company;
+USE record_company;
+
+CREATE TABLE bands (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE albums (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  release_year INT,
+  band_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (band_id) REFERENCES bands(id)
+);
+
+
+select * from bands;
+select * from albums;
+
+select id as 'ID', name as 'Name' from bands;
+select * from bands order by name desc;
+
+
+
+
+
+
+
